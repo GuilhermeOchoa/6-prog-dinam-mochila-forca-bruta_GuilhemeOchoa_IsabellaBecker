37e35e76084af5a71b81e4ab29948adc8103f926
Guilherme Ochoa <guilhermeochoa@gmail.com>
2024-09-08 23:51:42 -0300

    Update App.java

--- App.java
+++ App.java
@@ -7,18 +7,16 @@ public class App {
     private static long iterations = 0;
 
     public static void main(String[] args) {
-        int[] testValues = {4, 8, 16, 32, 128, 1000, 10000};
+        int[] testValues = {4, 8, 16, 32, 128, 1000};
         for (int n : testValues) {
             iterations = 0;
             long startTime = System.currentTimeMillis();
-            long result = fibo(n);
+            long result = memoizedFibo(n);
             long endTime = System.currentTimeMillis();
             System.out.println("FIBO(" + n + ") = " + result + ", Iterations: " + iterations + ", Time: " + (endTime - startTime) + " ms");
         }
     }
 
-
-    
     public static long fibo(int n) {
         if (n <= 1) {
             return n;
@@ -31,6 +29,35 @@ public class App {
             f[i] = f[i - 1] + f[i - 2];
         }
         return f[n];
+    }
+
+    public static long fiboRecursive(int n) {
+        iterations++;
+        if (n <= 1) {
+            return n;
+        }
+        return fiboRecursive(n - 1) + fiboRecursive(n - 2);
+    }
+
+    public static long memoizedFibo(int n) {
+        long[] memo = new long[n + 1];
+        for (int i = 0; i <= n; i++) {
+            memo[i] = -1;
+        }
+        return lookupFibo(memo, n);
+    }
+
+    private static long lookupFibo(long[] memo, int n) {
+        iterations++;
+        if (memo[n] >= 0) {
+            return memo[n];
+        }
+        if (n <= 1) {
+            memo[n] = n;
+        } else {
+            memo[n] = lookupFibo(memo, n - 1) + lookupFibo(memo, n - 2);
+        }
+        return memo[n];
     }
 
 
